0ddd858e224a73ab96fc3f00c1657099930d32a1
jessicaoo-git <jessi-0517@hotmail.com>
2023-09-15 08:33:10 -0500

    add layout to faqs

--- pages/preguntas-frecuentes/index.page.tsx
+++ pages/preguntas-frecuentes/index.page.tsx
@@ -4,44 +4,63 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-
-import { faqsData } from "dh-marvel/components/faqs/faqsData";
+import { faqsData, FaqsType } from 'dh-marvel/components/faqs/faqsData';
 import { GetStaticProps } from "next";
-import styles from './faqs.module.css'
+import styles from './faqs.module.css';
+import { NextPageWithLayout } from '../_app.page';
+import BodySingle from "dh-marvel/components/layouts/body/single/body-single";
+import LayoutGeneral from 'dh-marvel/components/layouts/layout-general';
+import { ReactElement } from 'react';
 
-const Faqs = ({ data }: any) =>  {
+
+interface Props {
+  data: FaqsType[]
+}
+
+const Faqs: NextPageWithLayout<Props> = ({ data }: Props) =>  {
   return (
-    <div className={styles.faqs__container}>
-      <Typography variant="h4">Preguntas Frecuentes</Typography>
-      <div>
-        {data?.map((faqs: any) => {
-          return (
-            <>
-              <Accordion key={faqs.id}>
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
-                >
-                  <Typography className={styles.faqs__question}>{faqs.question}</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography>
-                    {faqs.answer}
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
-
-            </>
-          );
-        })}
+   <>
+      
+      <BodySingle title={"Preguntas Frecuentes"}>
+      <div className={styles.faqs__container}>
+        
+        <div>
+          {data?.map((faqs: any) => {
+            return (
+              <>
+                <Accordion key={faqs.id}>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <Typography className={styles.faqs__question}>{faqs.question}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>
+                      {faqs.answer}
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+               
+              </>
+            );
+          })}
+        </div>
       </div>
-    </div>
+      </BodySingle>
+    </>
   );
 }
-export default Faqs
+
+
+Faqs.getLayout = function getLayout(page: ReactElement) {
+  return <LayoutGeneral>{page}</LayoutGeneral>
+}
 
 export const getStaticProps: GetStaticProps = async () => {
   const data = faqsData;
   return { props: { data } };
 };
+
+export default Faqs
